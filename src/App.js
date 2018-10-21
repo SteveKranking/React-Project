e760a83a1a03b48d4a41186e5b7f97424b91e2a5
Stephen Kranking <stephenkranking@yahoo.com>
2018-10-21 17:25:05 -0400

    using props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1> Hi I'm a React App! </h1>
+        <Person name="Bobert" age="1" />
+        <Person name="Aaron" age="2"> My hobbies: Racing </Person>
+        <Person name="Stephen" age="3" />
       </div>
     );
   }
